a2613fd0d219891186d846ea83ccf8659dbcf567
Andres Carvajal <jc1993box@hotmail.com>
2020-04-25 06:23:39 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
 i = 0 
 block_return_values = []
 while i < collection.length 
-block_return_values << yield (collection[i])
+block_return_values << yield(collection[i])
 i += 1
 end
 
@@ -15,19 +15,6 @@ else
 end
 end
 
-def my_all?(collection)
-  i = 0
-  block_return_values = []
-  while i < collection.length
-    block_return_values << yield(collection[i])
-    i = i + 1
-  end
- 
-  if block_return_values.include?(false)
-    false
-  else
-    true
-  end
-end
+
 
 my_all?([1,2,3]) {|i| i < 2}
